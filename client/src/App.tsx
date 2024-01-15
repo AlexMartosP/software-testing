9644a5ccb237b28472fd8025796d144c38a64dae
AlexMartosP <alex.martos@hotmail.se>
2024-01-15 17:46:10 +0100

    Add TW

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <>App</>;
+  return (
+    <div>
+      <h2>Add product</h2>
+      <form>
+        <input type="text" name="Name" />
+        <input type="number" name="price" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
